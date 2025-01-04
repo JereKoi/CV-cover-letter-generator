7a5d13d193b02a607e35d098b0c91332d223828d
Jere <jerekoivisto7@gmail.com>
2025-01-04 13:37:02 +0200

    Creating initial files and pages for the project to get started.

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+    return (
+      <div>
+        <h1>Welcome to CV and Cover Letter Generator!</h1>
+        <a href="/create">Start creating your CV</a>
+      </div>
+    );
+  };
+  export default HomePage;
+  
