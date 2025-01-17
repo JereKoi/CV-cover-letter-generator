7591b5585d22208a73ddee2d93f69b66513d5d25
Jere <jerekoivisto7@gmail.com>
2025-01-17 19:03:06 +0200

    Initial firebase setup.

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,14 +1,18 @@
-import { initializeApp } from "firebase/app";
+import admin from "firebase-admin";
 
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
+let app: admin.app.App;
 
-const app = initializeApp(firebaseConfig);
+// Check if Firebase app is already initialized
+if (!admin.apps.length) {
+  app = admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
+  });
+} else {
+  app = admin.app();
+}
 
-export default app;
+export const db = admin.firestore();
